8ccbd4f39c93bd026ae5f3dc5d150180b9a8cbc7
Pisut Oncharoen <ponchar2@z2t13c2.42bangkok.com>
2025-05-05 12:56:58 +0700

    update

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+num = float(input())
+if num > 0:
+    print("This number is positive.")
+elif num < 0:
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
