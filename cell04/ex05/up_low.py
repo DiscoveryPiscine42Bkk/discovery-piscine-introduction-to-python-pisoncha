6215159ca2746f2d16ddd839d51ee2063d2a847e
Pisut Oncharoen <ponchar2@z1t3c2.42bangkok.com>
2025-05-06 14:05:26 +0700

    update

--- cell04/ex05/up_low.py
+++ cell04/ex05/up_low.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 str_input = input()
-output = ""
-for c in str_input:
-    if c.isupper():
-        output += c.lower()
-    elif c.islower():
-        output += c.upper()
-    else:
-        output += c
-print(output)
+print(str_input.swapcase())
