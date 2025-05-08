ddbe280dbc90ce02245f7566736f7d3546e4abb9
Pisut Oncharoen <ponchar2@z2t13c2.42bangkok.com>
2025-05-08 15:03:43 +0700

    update

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -5,7 +5,9 @@ import sys
 argv = sys.argv 
 n = len(argv)
 if n == 3:
-    output = list(range(int(argv[1]), int(argv[2]) + 1))
+    n_min = min(int(argv[1]), int(argv[2]))
+    n_max = max(int(argv[1]), int(argv[2]))
+    output = list(range(n_min, n_max + 1))
     print(output)
 else:
     print("none")
